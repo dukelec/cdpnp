a8af9abe55ecbf490d24c187a15e1a3f87492129
Duke <d@d-l.io>
2021-06-07 23:35:46 +0800

    init

---
+++ pnp_cv.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+# This programs calculates the orientation of an object.
+# The input is an image, and the output is an annotated image
+# with the angle of otientation for each object (0 to 180 degrees)
+
+import sys, os
+import struct
+from time import sleep
+import _thread
+import re
+
+sys.path.append(os.path.join(os.path.dirname(__file__), './pycdnet'))
+
+from cdnet.utils.log import *
+from cdnet.utils.cd_args import CdArgs
+from cdnet.dev.cdbus_serial import CDBusSerial
+from cdnet.dev.cdbus_bridge import CDBusBridge
+from cdnet.dispatch import *
+ 
+import cv2 as cv
+from math import atan2, cos, sin, sqrt, pi
+import numpy as np
+
+
+sock_pic = None
+cur_pic = None
+pic_ready = False
+
+
+def cv_get_pos(img):
+    # Convert image to grayscale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+     
+    # Convert image to binary
+    _, bw = cv.threshold(gray, 50, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+     
+    # Find all the contours in the thresholded image
+    contours, _ = cv.findContours(bw, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+     
+    for i, c in enumerate(contours):
+     
+      # Calculate the area of each contour
+      area = cv.contourArea(c)
+     
+      # Ignore contours that are too small or too large
+      if area < 100 or 100000 < area:
+        continue
+     
+      # cv.minAreaRect returns:
+      # (center(x, y), (width, height), angle of rotation) = cv2.minAreaRect(c)
+      rect = cv.minAreaRect(c)
+      box = cv.boxPoints(rect)
+      box = np.int0(box)
+     
+      # Retrieve the key parameters of the rotated bounding box
+      center = (int(rect[0][0]),int(rect[0][1])) 
+      width = int(rect[1][0])
+      height = int(rect[1][1])
+      angle = int(rect[2])
+     
+         
+      if width < height:
+        angle = 90 - angle
+      else:
+        angle = -angle
+             
+      label = "  Rotation Angle: " + str(angle) + " degrees"
+      cv.drawContours(img,[box],0,(0,0,255),1)
+      #textbox = cv.rectangle(img, (center[0]-35, center[1]-25), 
+      #  (center[0] + 295, center[1] + 10), (255,255,255), -1)
+      cv.putText(img, label, (center[0]-50, center[1]), 
+        cv.FONT_HERSHEY_SIMPLEX, 0.4, (255,0,0), 1, cv.LINE_AA)
+
+
+def pic_rx():
+    global cur_pic, pic_ready
+    rx_dat = None
+    dat_cnt = 0
+    
+    while True:
+        rx = sock_pic.recvfrom()
+        #print('\x1b[0;37m  ' + re.sub(br'[^\x20-\x7e]',br'.', rx[0]).decode() + '\x1b[0m')
+        
+        hdr = rx[0][0]  # [5:4] FRAGMENT: 00: error, 01: first, 10: more, 11: last, [3:0]: cnt
+        dat = rx[0][1:]
+        
+        if hdr == 0x50:     # first
+            rx_dat = dat
+            dat_cnt = 0
+        
+        elif (hdr & 0xf0) == 0x60: # more
+            if dat_cnt == (hdr & 0xf):
+                rx_dat += dat
+            else:
+                print(f'pic, wrong cnt, local: {dat_cnt} != rx: {hdr & 0xf}, dat len: {len(dat)}')
+        
+        elif (hdr & 0xf0) == 0x70: # end
+            if dat_cnt == (hdr & 0xf):
+                #print('pic received!')
+                inp = np.asarray(bytearray(rx_dat), dtype=np.uint8)
+                img = cv.imdecode(inp, cv.IMREAD_COLOR)
+                img = cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
+                height, width = img.shape[:2]
+                cv_get_pos(img)
+                cur_pic = cv.drawMarker(img, (int(width/2),int(height/2)), color=(0,255,0), markerType=cv.MARKER_CROSS, thickness=1)
+                pic_ready = True
+            else:
+                print(f'pic, wrong cnt at end, local: {dat_cnt} != rx: {hdr & 0xf}, dat len: {len(dat)}')
+        
+        else:
+            print(f'pic, receive err, local: {dat_cnt}, rx: {hdr & 0xf}, all len: {len(img_dat)}')
+        
+        dat_cnt += 1
+        if dat_cnt == 0x10:
+            dat_cnt = 0
+
+
+def cv_window():
+    while True:
+        if not pic_ready:
+            sleep(0.1)
+            continue
+        cv.imshow('image', cur_pic)
+        cv.waitKey(10)
+
+def pnp_cv_init():
+    global sock_pic
+    sock_pic = CDNetSocket(('', 0x10))
+    _thread.start_new_thread(pic_rx, ())
+    _thread.start_new_thread(cv_window, ())
+
---
+++ pnp_main.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python3
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2017, DUKELEC, Inc.
+# All rights reserved.
+#
+# Author: Duke Fong <d@d-l.io>
+
+"""CDNET debug tool
+
+This tool use CDBUS Bridge by default, communicate with any node on the RS485 bus.
+<- 
+"""
+
+import sys, tty, os, termios
+import struct
+from time import sleep
+import _thread
+import re
+try:
+    import readline
+except:
+    from pyreadline import Readline
+    readline = Readline()
+
+sys.path.append(os.path.join(os.path.dirname(__file__), './pycdnet'))
+
+from cdnet.utils.log import *
+from cdnet.utils.cd_args import CdArgs
+from cdnet.dev.cdbus_serial import CDBusSerial
+from cdnet.dev.cdbus_bridge import CDBusBridge
+from cdnet.dispatch import *
+
+from pnp_cv import pnp_cv_init
+
+args = CdArgs()
+dev_str = args.get("--dev", dft="ttyACM0")
+#pos = int(args.get("--pos", dft="0"), 0)
+
+x = args.get("--x")
+y = args.get("--y")
+z = args.get("--z")
+r = args.get("--r")
+
+if args.get("--help", "-h") != None:
+    print(__doc__)
+    exit()
+
+if args.get("--verbose", "-v") != None:
+    logger_init(logging.VERBOSE)
+elif args.get("--debug", "-d") != None:
+    logger_init(logging.DEBUG)
+elif args.get("--info", "-i") != None:
+    logger_init(logging.INFO)
+
+
+dev = CDBusBridge(dev_str)
+CDNetIntf(dev, mac=0x00)
+sock = CDNetSocket(('', 0xcdcd))
+sock_dbg = CDNetSocket(('', 9))
+
+K_ESC = 27
+K_RET = 10
+
+K_UP = 65
+K_DOWN = 66
+K_LEFT = 68
+K_RIGHT = 67
+K_PAGEUP = 53
+K_PAGEDOWN = 54
+K_R = 114
+k_SHF_R = 82
+K_P = 112
+
+K_0 = 48
+K_INC = 61 # +
+K_DEC = 45 # -
+K_SPACE = 32
+
+def getkey():
+    old_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin.fileno())
+    try:
+        while True:
+            b = os.read(sys.stdin.fileno(), 3)
+            k = b[2] if len(b) == 3 else b[0]
+            return k
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+
+def dbg_echo():
+    while True:
+        rx = sock_dbg.recvfrom()
+        #print('\x1b[0;37m  ' + re.sub(br'[^\x20-\x7e]',br'.', rx[0][5:-1]).decode() + '\x1b[0m')
+        print('\x1b[0;37m  ' + re.sub(br'[^\x20-\x7e]',br'.', rx[0]).decode() + '\x1b[0m')
+
+_thread.start_new_thread(dbg_echo, ())
+pnp_cv_init()
+
+
+print('start...')
+
+# 4mm / (50 * 16 (md: 2)) = 0.005mm per micro step
+# 360' / (50 * 16 (md: 2)) = 0.45' per micro step
+DIV_MM2STEP = 0.005
+DIV_DEG2STEP = 0.45
+
+grab_ofs = [20, 0]  # grab offset to camera
+
+pcb_origin = [0, 0]
+pcb_angle = 0.0
+
+fiducial = [
+    [2.0, 2.0],     # point 0 (main)
+    [42.0, 42.0],   # point 1 (calc angle)
+]
+
+pos = {}
+
+import csv
+with open('pos.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in spamreader:
+        if row[0] == 'Ref':
+            continue
+        
+        if row[2] in pos:
+            if row[1] in pos[row[2]]:
+                pos[row[2]][row[1]].append(row)
+            else:
+                pos[row[2]][row[1]] = [row]
+        else:
+            pos[row[2]] = {}
+            pos[row[2]][row[1]] = [row]
+
+
+#print(pos)
+
+def motor_enable():
+    for i in range(5):
+        print(f'motor enable: #{i+1}')
+        sock.sendto(b'\x20'+struct.pack("<H", 0x00d6) + struct.pack("<B", 1), (f'80:00:0{i+1}', 0x5))
+        rx = sock.recvfrom(timeout=1)
+        print('motor enable ret: ' + rx[0].hex(), rx[1])
+
+
+def goto_pos(pos):
+    retry_cnt = 0
+    done_flag = [0, 0, 0, 0, 0]
+    while True:
+        if not done_flag[0]:
+            sock.sendto(b'\x20'+struct.pack("<i", round(pos[0]/DIV_MM2STEP)), ('80:00:03', 0x6))
+        if (not done_flag[1]) or (not done_flag[2]):
+            sock.sendto(b'\x20'+struct.pack("<i", round(pos[1]/DIV_MM2STEP)), ('80:00:e0', 0x6))
+        if not done_flag[2]:
+            sock.sendto(b'\x20'+struct.pack("<i", round(pos[2]/DIV_MM2STEP)), ('80:00:04', 0x6))
+        if not done_flag[3]:
+            sock.sendto(b'\x20'+struct.pack("<i", round(pos[3]/DIV_DEG2STEP)), ('80:00:05', 0x6))
+        
+        for i in range(5 - (done_flag[0] + done_flag[1] + done_flag[2] + done_flag[3] + done_flag[4])):
+            dat, src = sock.recvfrom(timeout=0.5)
+            if src:
+                if src[0] == '80:00:01':
+                    done_flag[0] = 1
+                if src[0] == '80:00:02':
+                    done_flag[1] = 1
+                if src[0] == '80:00:03':
+                    done_flag[2] = 1
+                if src[0] == '80:00:04':
+                    done_flag[3] = 1
+                if src[0] == '80:00:05':
+                    done_flag[4] = 1
+        if done_flag[0] and done_flag[1] and done_flag[2] and done_flag[3] and done_flag[4]:
+            break
+        print(f'error: retry_cnt: {retry_cnt}, done_flag: f{done_flag}')
+        retry_cnt += 1
+        if retry_cnt > 3:
+            print(f'error: set retry > 3, done_flag: f{done_flag}')
+            return -1
+    
+    return 0
+    
+    retry_cnt = 0
+    tgt = 1
+    while True:
+        sock.sendto(b'\x00'+struct.pack("<H", 0x00d7) + struct.pack("<B", 1), (f'80:00:0{tgt}', 0x5))
+        dat, src = sock.recvfrom(timeout=0.5)
+        if src == None:
+            print(f'error: retry_cnt: {retry_cnt}')
+            retry_cnt += 1
+            if retry_cnt > 3:
+                print('error: poll retry > 3')
+                return -1
+            continue
+        retry_cnt = 0
+        if dat[0] == 0x80 and dat[1] == 0:
+            tgt += 1
+            if tgt > 5:
+                return 0
+        sleep(0.1)
+
+
+motor_enable()
+
+
+del_pow = 0 # + - by key
+cur_pos = [0, 0, 0, 0] # x, y, z, r
+cur_cam = 0
+cur_pump = 0
+
+while True:
+    k = getkey()
+    print(k)
+    if k == K_ESC:
+        break
+    if k == K_RET:
+        pass
+    
+    if k == K_DOWN:
+        cur_pos[1] += pow(10, del_pow)/100
+    elif k == K_UP:
+        cur_pos[1] -= pow(10, del_pow)/100
+    elif k == K_LEFT:
+        cur_pos[0] -= pow(10, del_pow)/100
+    elif k == K_RIGHT:
+        cur_pos[0] += pow(10, del_pow)/100
+    elif k == K_PAGEUP:
+        cur_pos[2] -= pow(10, del_pow)/100
+    elif k == K_PAGEDOWN:
+        cur_pos[2] += pow(10, del_pow)/100
+    elif k == K_R:
+        cur_pos[3] += pow(10, del_pow)/100
+    elif k == k_SHF_R:
+        cur_pos[3] -= pow(10, del_pow)/100
+    
+    if k == K_INC or k == K_DEC:
+        del_pow += (1 if k == K_INC else -1)
+        del_pow = max(0, min(del_pow, 4))
+        print(f'del_pow: {del_pow}')
+    
+    if k == K_SPACE:
+        cur_cam = 255 if cur_cam == 0 else 0
+        print('set cam...', cur_cam)
+        sock.sendto(b'\x20'+struct.pack("<H", 0x0036) + struct.pack("<B", cur_cam), ('80:00:10', 0x5))
+        rx = sock.recvfrom(timeout=1)
+        print('set cam ret: ' + rx[0].hex(), rx[1])
+    
+    if k == K_P:
+        cur_pump = 2 if cur_pump == 0 else 1
+        print('set pump...', cur_pump)
+        sock.sendto(b'\x20'+struct.pack("<H", 0x0036) + struct.pack("<B", cur_pump), ('80:00:11', 0x5))
+        rx = sock.recvfrom(timeout=1)
+        print('set pump ret: ' + rx[0].hex(), rx[1])
+        if cur_pump == 1:
+            sleep(0.5)
+            cur_pump = 0
+            print('set pump...', cur_pump)
+            sock.sendto(b'\x20'+struct.pack("<H", 0x0036) + struct.pack("<B", cur_pump), ('80:00:11', 0x5))
+            rx = sock.recvfrom(timeout=1)
+            print('set pump ret: ' + rx[0].hex(), rx[1])
+    
+    print(f'goto: {cur_pos[0]:.3f} {cur_pos[1]:.3f} {cur_pos[2]:.3f} {cur_pos[3]:.3f}')
+    goto_pos(cur_pos)
+
+
+print('exit...')
+exit()
+
+
+
+print('run...')
+#sock.sendto(b'\x20'+struct.pack("<i", pos) + struct.pack("<i", speed), ('80:00:ff', 0x6))
+
+if x != None:
+    x = int(x, 0)
+    sock.sendto(b'\x20'+struct.pack("<H", 0x00d6) + struct.pack("<B", 1), ('80:00:03', 0x5))
+    rx = sock.recvfrom()
+    print('\r-> ' + rx[0].hex(), rx[1])
+    sock.sendto(b'\x20'+struct.pack("<i", x), ('80:00:03', 0x6))
+    rx = sock.recvfrom()
+    print('\rset x ret: ' + rx[0].hex(), rx[1])
+
+if y != None:
+    y = int(y, 0)
+    sock.sendto(b'\x20'+struct.pack("<H", 0x00d6) + struct.pack("<B", 1), ('80:00:e0', 0x5))
+    rx = sock.recvfrom()
+    print('\r-> ' + rx[0].hex(), rx[1])
+    sock.sendto(b'\x20'+struct.pack("<i", y), ('80:00:e0', 0x6))
+    rx = sock.recvfrom()
+    print('\rset z ret: ' + rx[0].hex(), rx[1])
+
+if z != None:
+    z = int(z, 0)
+    sock.sendto(b'\x20'+struct.pack("<H", 0x00d6) + struct.pack("<B", 1), ('80:00:04', 0x5))
+    rx = sock.recvfrom()
+    print('\r-> ' + rx[0].hex(), rx[1])
+    sock.sendto(b'\x20'+struct.pack("<i", z), ('80:00:04', 0x6))
+    rx = sock.recvfrom()
+    print('\rset z ret: ' + rx[0].hex(), rx[1])
+
+if r != None:
+    r = int(r, 0)
+    sock.sendto(b'\x20'+struct.pack("<H", 0x00d6) + struct.pack("<B", 1), ('80:00:05', 0x5))
+    rx = sock.recvfrom()
+    print('\r-> ' + rx[0].hex(), rx[1])
+    sock.sendto(b'\x20'+struct.pack("<i", r), ('80:00:05', 0x6))
+    rx = sock.recvfrom()
+    print('\rset r ret: ' + rx[0].hex(), rx[1])
+
+print('exit...')
