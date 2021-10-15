3b75d43bcd57fc8d1f6b74a473a4ab3b76a550bd
Duke <d@d-l.io>
2021-10-15 20:51:46 +0800

    speedup rotate

--- pnp_xyz.py
+++ pnp_xyz.py
@@ -74,16 +74,12 @@ def enable_motor():
         xyz['logger'].info('motor enable ret: ' + rx.hex())
         
         xyz['logger'].info(f'motor set accel #{i+1}')
-        rx = cd_reg_rw(f'80:00:0{i+1}', 0x00c4, struct.pack("<I", 800000))
+        rx = cd_reg_rw(f'80:00:0{i+1}', 0x00c4, struct.pack("<I", 800000 if i != 4 else 20000))
         xyz['logger'].info('motor set ret: ' + rx.hex())
         
         xyz['logger'].info(f'motor set vref #{i+1}')
-        rx = cd_reg_rw(f'80:00:0{i+1}', 0x00ae, struct.pack("<H", 600))
+        rx = cd_reg_rw(f'80:00:0{i+1}', 0x00ae, struct.pack("<H", 600 if i != 4 else 300))
         xyz['logger'].info('motor set vref ret: ' + rx.hex())
-    
-    xyz['logger'].info(f'motor set accel #5')
-    rx = cd_reg_rw(f'80:00:05', 0x00c4, struct.pack("<I", 2000))
-    xyz['logger'].info('motor set ret: ' + rx.hex())
 
 
 def wait_stop():
@@ -120,7 +116,7 @@ def goto_pos(pos, wait=False, s_speed=260000):
     retry_cnt = 0
     done_flag = [0, 0, 0, 0, 0]
     m_vector = max(math.sqrt(math.pow(delta[0], 2) + math.pow(delta[1], 2) + math.pow(delta[2], 2)), 0.01)
-    v_speed = [round(s_speed * abs(delta[0])/m_vector), round(s_speed * abs(delta[1])/m_vector), round(s_speed * abs(delta[2])/m_vector), round(s_speed / 20)]
+    v_speed = [round(s_speed * abs(delta[0])/m_vector), round(s_speed * abs(delta[1])/m_vector), round(s_speed * abs(delta[2])/m_vector), round(s_speed / 5)]
     b_speed = [struct.pack("<i", v_speed[0]), struct.pack("<i", v_speed[1]), struct.pack("<i", v_speed[2]), struct.pack("<i", v_speed[3])]
     
     while True:
